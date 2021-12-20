1228fada4f4e4ba6bce358e8906b3f458f23865b
S.M.IRAMUL MURSHED <35691070+iramulmurshed@users.noreply.github.com>
2021-12-21 00:27:04 +0600

    nav updated with name

--- final-frontend/src/seller/nav/Nav.js
+++ final-frontend/src/seller/nav/Nav.js
@@ -2,15 +2,35 @@ import React,{useState,useEffect} from 'react';
 import Logout from "../logout/Logout";
 import {Link, Redirect} from "react-router-dom";
 import "./nav.css"
+import axios from "axios";
 
 const Nav = () => {
+    let [name,setName] =useState("")
+    // useEffect(() => {
 
+
+            let obj = JSON.parse(localStorage.getItem('seller'));
+            let id = obj.userId;
+            let url = `/profile/${id}`
+            console.log(url);
+            axios
+                .get(url)
+                .then((res) => {
+                    let p = res.data;
+                    setName(p.s_name);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+
+    // }, []);
     return (
         <div className="d-flex ">
             <p>E-SHOP</p>
             <div className="d-flex">
 
             <Link to="/dashboard"><button className="btn btn-primary">Seller Dashboard</button></Link>
+                <p>{name}</p>
                 <Logout/>
             </div>
         </div>
